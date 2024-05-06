7adf8346f7456fb9fb9513370a4ce6b02a94275e
Gaurav <gaurav-nelson@users.noreply.github.com>
2024-05-06 22:14:11 +1000

    Added tests for checking relative links

--- index.test.js
+++ index.test.js
@@ -42,3 +42,42 @@ test("linkspector should check image links in Markdown file", async () => {
 });
 
 
+test("linkspector should check relative links in Markdown file", async () => {
+  let hasErrorLinks = false;
+  let currentFile = ""; // Variable to store the current file name
+  let results = []; // Array to store the results if json is true
+
+  for await (const { file, result } of linkspector(
+    "./test/fixtures/relative/.relativeTest.yml",
+    cmd
+  )) {
+    currentFile = file;
+    for (const linkStatusObj of result) {
+      if (cmd.json) {
+        results.push({
+          file: currentFile,
+          link: linkStatusObj.link,
+          status_code: linkStatusObj.status_code,
+          line_number: linkStatusObj.line_number,
+          position: linkStatusObj.position,
+          status: linkStatusObj.status,
+          error_message: linkStatusObj.error_message,
+        });
+      }
+      if (linkStatusObj.status === "error") {
+        hasErrorLinks = true;
+      }
+    }
+  }
+
+  expect(hasErrorLinks).toBe(true);
+  expect(results.length).toBe(7);
+  expect(results[0].status).toBe("alive");
+  expect(results[1].status).toBe("alive");
+  expect(results[2].status).toBe("alive");
+  expect(results[3].status).toBe("alive");
+  expect(results[4].status).toBe("alive");
+  expect(results[5].status).toBe("alive");
+  expect(results[6].status).toBe("error");
+}
+);
---
+++ test/fixtures/relative/relative2.md
@@ -0,0 +1,17 @@
+# Relative 2 Heading Level One
+
+This is a paragraph in the first file in a first level heading.
+
+[Link to Relative 1 Heading Level One](relative1.md#relative-1-heading-level-one)
+
+## <a name="custom-id-level-two"></a> Relative 2 Heading Level Two
+
+This is a paragraph in the first file in a second level heading.
+
+[Link to Relative 1 Heading Level Two](relative1.md#relative-1-heading-level-two)
+
+### Relative 2 Heading Level Three {#custom-id-level-three}
+
+This is a paragraph in the first file in a third level heading.
+
+[Link to Relative 1 Heading Level Three](relative1.md#relative-1-heading-level-three)
