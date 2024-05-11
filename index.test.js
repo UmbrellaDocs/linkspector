8db240e8ba5bee27593f2cfd400c46dfc5390966
Gaurav <gaurav-nelson@users.noreply.github.com>
2024-05-11 20:46:03 +1000

    Chore: Put markdown fixtures in a common folder, add test for updateLinkStatusObj function

--- index.test.js
+++ index.test.js
@@ -11,7 +11,7 @@ test("linkspector should check image links in Markdown file", async () => {
   let results = []; // Array to store the results if json is true
 
   for await (const { file, result } of linkspector(
-    "./test/fixtures/image/imageTest.yml",
+    "./test/fixtures/markdown/image/imageTest.yml",
     cmd
   )) {
     currentFile = file;
@@ -47,7 +47,7 @@ test("linkspector should check relative links in Markdown file", async () => {
   let results = []; // Array to store the results if json is true
 
   for await (const { file, result } of linkspector(
-    "./test/fixtures/relative/.relativeTest.yml",
+    "./test/fixtures/markdown/relative/.relativeTest.yml",
     cmd
   )) {
     currentFile = file;
@@ -111,4 +111,40 @@ test("linkspector should check top-level relative links in Markdown file", async
 
   expect(hasErrorLinks).toBe(false);
   expect(results.length).toBe(4);
+});
+
+test("linkspector should add back the removed duplicates when returning the results", async () => {
+  let hasErrorLinks = false;
+  let currentFile = ""; // Variable to store the current file name
+  let results = []; // Array to store the results if json is true
+
+  for await (const { file, result } of linkspector(
+    "./test/fixtures/markdown/duplicates/duplicateTest.yml",
+    cmd
+  )) {
+    currentFile = file;
+    for (const linkStatusObj of result) {
+      if (cmd.json) {
+        results.push({
+          file: currentFile,
+          link: linkStatusObj.link,
+          status_code: linkStatusObj.status_code,
+          line_number: linkStatusObj.line_number,
+          position: linkStatusObj.position,
+          status: linkStatusObj.status,
+          error_message: linkStatusObj.error_message,
+        });
+      }
+      if (linkStatusObj.status === "error") {
+        hasErrorLinks = true;
+      }
+    }
+  }
+
+  expect(hasErrorLinks).toBe(true);
+  expect(results.length).toBe(4);
+  expect(results[0].status).toBe("alive");
+  expect(results[1].status).toBe("error");
+  expect(results[2].status).toBe("alive");
+  expect(results[3].status).toBe("error");
 });
---
+++ test/fixtures/markdown/duplicates/duplicate1.md
@@ -0,0 +1,7 @@
+This is a link to [Google](https://www.google.com).
+
+this is a link to [Yahoo](https://www.yahoo434234esdsadasd.com).
+
+This is a link to [Google](https://www.google.com) again.
+
+this is a link to [Yahoo](https://www.yahoo434234esdsadasd.com) again.
