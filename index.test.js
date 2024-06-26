8ffb414a3e6918c48bb5222de17b9e0f03a405e6
Gaurav Nelson <23069445+gaurav-nelson@users.noreply.github.com>
2024-06-26 16:19:30 +1000

    fix:bug fix for checking HTML encoded section links

--- index.test.js
+++ index.test.js
@@ -220,3 +220,37 @@ test('linkspector should check HTML encoded section links', async () => {
   expect(results[2].status).toBe('alive')
   expect(results[3].status).toBe('alive')
 })
+
+test('linkspector should check HTML encoded section links and include anchor names', async () => {
+  let hasErrorLinks = false
+  let currentFile = '' // Variable to store the current file name
+  let results = [] // Array to store the results if json is true
+
+  for await (const { file, result, headings } of linkspector(
+    './test/fixtures/markdown/with-html-anchors/.withHtmlAnchorsTest.yml',
+    cmd
+  )) {
+    currentFile = file
+    for (const linkStatusObj of result) {
+      if (cmd.json) {
+        results.push({
+          file: currentFile,
+          link: linkStatusObj.link,
+          status_code: linkStatusObj.status_code,
+          line_number: linkStatusObj.line_number,
+          position: linkStatusObj.position,
+          status: linkStatusObj.status,
+          error_message: linkStatusObj.error_message,
+        })
+      }
+      if (linkStatusObj.status === 'error') {
+        hasErrorLinks = true
+      }
+    }
+  }
+
+  // Test expectations for link checks
+  expect(hasErrorLinks).toBe(false)
+  expect(results.length).toBe(1)
+  expect(results[0].status).toBe('alive')
+})
---
+++ test/fixtures/markdown/with-html-anchors/html-anchor.md
@@ -0,0 +1,9 @@
+# This is heading 1
+
+This is a paragraph in the first file in a first level heading.
+
+Anchor with `a` <a name="custom-id-level-one"></a>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel mauris sit amet ipsum venenatis placerat.
+
+Link to anchor with `a` [Link to custom id level one](#custom-id-level-one).
