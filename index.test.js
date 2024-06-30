cece67b6972f947a021edb47746342635b496e50
Gaurav Nelson <23069445+gaurav-nelson@users.noreply.github.com>
2024-07-01 07:24:24 +1000

    fix: Update linkspector to check links to headings with inline bold, italic, code, link, and image elements

--- index.test.js
+++ index.test.js
@@ -226,7 +226,7 @@ test('linkspector should check HTML encoded section links and include anchor nam
   let currentFile = '' // Variable to store the current file name
   let results = [] // Array to store the results if json is true
 
-  for await (const { file, result, headings } of linkspector(
+  for await (const { file, result } of linkspector(
     './test/fixtures/markdown/with-html-anchors/.withHtmlAnchorsTest.yml',
     cmd
   )) {
@@ -253,4 +253,36 @@ test('linkspector should check HTML encoded section links and include anchor nam
   expect(hasErrorLinks).toBe(false)
   expect(results.length).toBe(1)
   expect(results[0].status).toBe('alive')
+})
+
+test('linkspector should check links to headings with inline bold, italic, code, link, and image elements', async () => {
+  let hasErrorLinks = false
+  let currentFile = '' // Variable to store the current file name
+  let results = [] // Array to store the results if json is true
+
+  for await (const { file, result } of linkspector(
+    './test/fixtures/markdown/headings/headingsTest.yml',
+    cmd
+  )) {
+    currentFile = file
+    for (const linkStatusObj of result) {
+      if (cmd.json) {
+        results.push({
+          file: currentFile,
+          link: linkStatusObj.link,
+          status_code: linkStatusObj.status_code,
+          line_number: linkStatusObj.line_number,
+          position: linkStatusObj.position,
+          status: linkStatusObj.status,
+          error_message: linkStatusObj.error_message,
+        })
+      }
+      if (linkStatusObj.status === 'error') {
+        hasErrorLinks = true
+      }
+    }
+  }
+
+  expect(hasErrorLinks).toBe(false)
+  expect(results.length).toBe(8)
 })
